e18d831189b2b08c2f18a917949d378b99aa3de0
nankotsu <nankotsu230@gmail.com>
2013-06-05 18:15:15 +0900

    add sort method

--- nk0t/mods/usefulchest/UsefulChestUtilities.scala
+++ nk0t/mods/usefulchest/UsefulChestUtilities.scala
@@ -1,8 +1,24 @@
 package nk0t.mods.usefulchest
 
+import net.minecraft.item.ItemStack
+
 object UsefulChestUtilities {
 
     def sortUsefulChest(usefulchest : TileEntityUsefulChest) = {
-    	println("sort")
+
+        val sortedItems = usefulchest.usefulChestContents.filter(_ != null)
+            .sortWith((a, b) => {
+
+                if (a.itemID == b.itemID) {
+                    a.getItemDamage() < b.getItemDamage()
+                }
+                else {
+                    a.itemID < b.itemID
+                }
+            })
+
+        usefulchest.usefulChestContents = new Array[ItemStack](usefulchest.getSizeInventory())
+
+        sortedItems.copyToArray(usefulchest.usefulChestContents)
     }
 }
