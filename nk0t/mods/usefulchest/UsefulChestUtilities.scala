d774087823c72b2f70fc43a05b670f4bd20285ee
nankotsu <nankotsu230@gmail.com>
2013-06-05 20:08:37 +0900

    add mergeStack method

--- nk0t/mods/usefulchest/UsefulChestUtilities.scala
+++ nk0t/mods/usefulchest/UsefulChestUtilities.scala
@@ -19,6 +19,35 @@ object UsefulChestUtilities {
 
         usefulchest.usefulChestContents = new Array[ItemStack](usefulchest.getSizeInventory())
 
-        sortedItems.copyToArray(usefulchest.usefulChestContents)
+        val mergedItems = mergeStack(sortedItems)
+
+        mergedItems.copyToArray(usefulchest.usefulChestContents)
+    }
+
+    def mergeStack(sortedItems : Array[ItemStack]) : Array[ItemStack] = {
+
+        var mergedItems = List.empty[ItemStack]
+
+        for (itemstack <- sortedItems) {
+
+            if (0 < mergedItems.size && mergedItems.last.itemID == itemstack.itemID &&
+                mergedItems.last.getItemDamage() == itemstack.getItemDamage()) {
+
+                val size = mergedItems.last.getMaxStackSize() - mergedItems.last.stackSize
+                if (size >= itemstack.stackSize) {
+                    mergedItems.last.stackSize += itemstack.stackSize
+                }
+                else {
+                    mergedItems.last.stackSize += size
+                    itemstack.stackSize -= size
+                    mergedItems = mergedItems :+ itemstack
+                }
+            }
+            else {
+                mergedItems = mergedItems :+ itemstack
+            }
+        }
+
+        return mergedItems.toArray[ItemStack]
     }
 }
